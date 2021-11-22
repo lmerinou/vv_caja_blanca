c4037b9b4ea5c8d0d6c4ae539588f49f913ec675
Luis Merino <lmerinoulizarna@gmail.com>
2021-11-22 11:51:31 +0100

    clase prueba y fichero de texto de ejemplo

--- src/main/java/com/practica/cajablanca/Editor.java
+++ src/main/java/com/practica/cajablanca/Editor.java
@@ -196,6 +196,7 @@ public class Editor {
     }
 
     public static void main(String[] args) throws EmptyCollectionException {
-
+        Editor editor = new Editor();
+        editor.leerFichero();
     }
 }
