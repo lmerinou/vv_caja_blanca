42a3f19ce9a68dde6ef4ef9d4c896c03e2b53c94
Bipbopbup <lucas.marco.pardo@alumnos.upm.es>
2021-11-22 12:27:16 +0100

    Creado test mayorLongitud 1.0

--- src/test/java/com/practica/cajablanca/MayorLongitudTest.java
+++ src/test/java/com/practica/cajablanca/MayorLongitudTest.java
@@ -2,6 +2,27 @@ package com.practica.cajablanca;
 
 import com.cajanegra.EmptyCollectionException;
 import org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class MayorLongitudTest {
+
+    @BeforeEach
+    public void initialization(){
+        Editor editor = new Editor();
+        editor.leerFichero("src/main/java/com/practica/cajablanca/miTexto.txt");
+    }
+    @Test
+    public void emptyEditor(){
+        Editor editor = new Editor();
+        assertThrows(EmptyCollectionException.class, editor::numPalabras);
+    }
+
+    @Test
+    public void inicioVacioTest(){
+        Editor editor = new Editor();
+        assertThrows(IllegalArgumentException.class, () -> editor.numPalabras(1,2,"hola") );
+    }
+}
