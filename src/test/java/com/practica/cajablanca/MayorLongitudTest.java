ec8057a6ff593c38dc4221404f17f5aa4bb0b129
Bipbopbup <lucas.marco.pardo@alumnos.upm.es>
2021-11-22 11:59:45 +0100

    Creado test mayorLongitud

---
+++ src/test/java/com/practica/cajablanca/MayorLongitudTest.java
@@ -0,0 +1,7 @@
+package com.practica.cajablanca;
+
+import com.cajanegra.EmptyCollectionException;
+import org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
